d6cb55edf2e15f3143db13fafd22cb92dca5cdbd
Elaine Ponsoni <elaineponsoni2@gmail.com>
2019-10-30 23:11:45 -0300

    finalizei o arquivo

--- loops.rb
+++ loops.rb
@@ -1,3 +1,23 @@
-5.times do |i|
-    puts 'repete a mensagem' + i.to_s + ' ves(es)'
- end
+# 5.times do |i|
+#     puts 'repete a mensagem' + i.to_s + ' ves(es)'
+#  end
+
+# init = 0
+# while init <= 10 do
+#     puts 'repete a mensagem ' + init.to_s + ' ves(es)'
+#     init += 1
+# end
+
+# for item in (0...10)
+#     puts 'repete a mensagem ' + item.to_s + ' ves(es)'
+# end
+
+#ARRAYS
+
+vingadores = ["Ironman", "Hulk", "Spiderman", "Thor"]
+#puts vingadores.class
+vingadores.each do |v|
+  puts v
+end
+
+puts "olá mundo"
