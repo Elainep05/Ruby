5ec32bb9fff16ff5454265528e3f7c2fbddd1bb2
Elaine Ponsoni <elaine.ponsoni@castgroup.com.br>
2020-01-29 14:48:07 -0300

    Nova aula validadores e matchers

--- unitarios/spec/avengers_spec.rb
+++ unitarios/spec/avengers_spec.rb
@@ -9,12 +9,13 @@ class AvengersHeadQuarter
 
 
     def put(avenger)
-        puts self.list.class
+        #puts self.list.class
         self.list.push(avenger)
     end 
     
 end
 
+#TDD - desenvolvimento guiado por testes
 describe AvengersHeadQuarter do
     
     it 'Deve adicionar um vingador' do
@@ -22,6 +23,48 @@ describe AvengersHeadQuarter do
 
         hq.put('Spiderman')
         expect(hq.list).to eql ['Spiderman']
+        # pode colcoar o include para comparar com a string
     end 
+
+    it 'deve adicionar uma lista de vingadores' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        res = hq.list.size > 0
+
+        expect(hq.list.size).to be > 0
+        expect(res).to be true
+
+    end
+
+    it 'thor deve ser o primeiro da lista' do
+        hq = AvengersHeadQuarter.new
+
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+        expect(hq.list).to start_with('Thor')
+    end
+
+    it 'Ironman deve ser o ultimo da lista' do
+        hq = AvengersHeadQuarter.new
+
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+        hq.put('Ironman')
+
+        expect(hq.list).to end_with('Ironman')
+    end
+
+    it 'deve conter o sobrenome' do
+        avenger = 'Peter Parker'
+
+        expect(avenger).to match(/Parker/)
+        expect(avenger).not_to match(/Elaine/)
+
+    end
 
 end
