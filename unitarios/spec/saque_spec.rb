021fc18655de9e27f3bb5af749d91190a8ef83ff
Elaine Ponsoni <elaine.ponsoni@castgroup.com.br>
2020-02-06 16:56:56 -0300

    atualização 0602

--- unitarios/spec/saque_spec.rb
+++ unitarios/spec/saque_spec.rb
@@ -1,5 +1,5 @@
 class ContaCorrente
-    attr_accessor :saldo
+    attr_accessor :saldo, :mensagem
 
     def initialize(saldo)
         self.saldo = saldo
@@ -7,7 +7,11 @@ class ContaCorrente
     end
 
     def saca(valor)
-        self.saldo = saldo
+        if  (valor > self.saldo)
+            self.mensagem = 'Saldo insuficiente para saque!'
+        else
+            self.saldo -= valor
+        end
         
     end
 
@@ -18,12 +22,51 @@ describe ContaCorrente do
     
     describe 'Saque' do
         context 'quando o valor é positivo' do
-            before(:all) do #metodo no contexto
+            before(:all) do 
                 @conta = ContaCorrente.new(1000.00)
-                @conta.saca(200) #@ instancia o metodo
+                @conta.saca(200.00) 
             end
             it 'então atualiza saldo' do
-                expect(@conta.saldo).to eql 800.00
+                expect(@conta.saldo) == 800.00
+            end
+        end
+
+        context 'quando o saldo é zero' do
+            before(:all) do
+                @conta = ContaCorrente.new(0.00)
+                @conta.saca(100.00)
+            end
+            it 'vejo mensagem' do
+                expect(@conta.mensagem) == 'Saldo insuficiente para saque!'
+            end
+            it 'Meu saldo permanece com zeros' do
+                expect(@conta.saldo) == 0.00
+            end
+        end
+
+        context 'quando o saldo é insuficiente' do
+            before(:all) do
+                @conta = ContaCorrente.new(100.00)
+                @conta.saca(101.00)
+            end
+            it 'vejo mensagem' do
+                expect(@conta.mensagem) == 'Saldo insuficiente para saque!'
+            end
+            it 'Meu saldo permanece conforme o valor inicial' do
+                expect(@conta.saldo) == 100.00
+            end
+        end
+
+        context 'quando o valor do saque é maior que o limite por transação' do
+            before(:all) do
+                @conta = ContaCorrente.new(1000.00)
+                @conta.saca(701.00)
+            end
+            it 'vejo mensagem' do
+                expect(@conta.mensagem) == 'Limite máximo por saque é de R$ 700'
+            end
+            it 'Meu saldo permanece conforme o valor inicial' do
+                expect(@conta.saldo) == 1000.00
             end
         end
     end
